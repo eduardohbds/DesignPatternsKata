d4f67eff0f873e7f1fc24bca165b6db8f3654d8a
eduardohbds <eduardo.hbds@gmail.com>
2023-10-18 17:12:04 -0300

    index the files

---
+++ GRASP/Creator/Frota/TS/index.ts
@@ -0,0 +1,6 @@
+import Caminhao from "./Caminhao"
+import Carro from "./Carro"
+import Frota from "./Frota"
+import Funcionario from "./Funcionario"
+import Moto from "./Moto"
+import Veiculo from "./Veiculo"
